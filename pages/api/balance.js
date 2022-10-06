216fd812237ce94806650add8b19b229de6ddd0d
BT_kung <u.nantanat@gmail.com>
2022-10-06 14:58:04 +0700

    finished

--- pages/api/balance.js
+++ pages/api/balance.js
@@ -5,11 +5,20 @@ export default function balanceRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
     const user = checkToken(req);
-    // return res.status(403).json({ok: false,message: "You do not have permission to check balance",});
-
+    if (!user || user.isAdmin) {
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to check balance",
+      });
+    }
     const users = readUsersDB();
-    //find user in DB and get their money value
+    const idx = users.findIndex((x) => x.username === user.username);
 
+    //find user in DB and get their money value
+    return res.json({
+      ok: true,
+      money: users[idx].money,
+    });
     //return response
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,33 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({ ok: false, message: "Permission denied" });
+    }
     //compute DB summary
+    const users = readUsersDB();
+    let countCustomer = 0;
+    let countAdmin = 0;
+    let sumMoney = 0;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].isAdmin) {
+        countAdmin++;
+      } else {
+        countCustomer++;
+        sumMoney += users[i].money;
+      }
+    }
     //return response
+    return res.json({
+      ok: true,
+      userCount: countCustomer,
+      adminCount: countAdmin,
+      totalMoney: sumMoney,
+    });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
